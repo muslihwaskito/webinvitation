d18613ff16079a555536e7b7de4a508a099089ac
Muhammad Muslih W <muslihwaskito@gmail.com>
2022-07-09 20:25:17 +0700

    add xml

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
-<html lang="en-US" prefix="og: https://ogp.me/ns#">
+<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:og="http://ogp.me/ns#"
+      xmlns:fb="https://www.facebook.com/2008/fbml">
 
 <head>
 	<meta charset="UTF-8">
